fa683e8f05c07d327fb470827d32d3511cb8d7c7
Dhaval Patel <dhaval.patel@agileinfoways.com>
2019-12-17 11:52:20 +0530

    move imagePicker Version 0.6.2

--- packages/image_picker/ios/Classes/ImagePickerPlugin.m
+++ packages/image_picker/ios/Classes/ImagePickerPlugin.m
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "FLTImagePickerPlugin.h"
+#import "ImagePickerPlugin.h"
 
 #import <AVFoundation/AVFoundation.h>
 #import <MobileCoreServices/MobileCoreServices.h>
